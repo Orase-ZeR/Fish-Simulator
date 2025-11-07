b30d1a5b1d99b62e206427f0440a80f696d5fde4
Orase-ZeR <142258201+Orase-ZeR@users.noreply.github.com>
2025-11-08 00:01:42 +0100

    Add files via upload

---
+++ assets/maps/ZELDA.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="ZELDA" tilewidth="16" tileheight="16" tilecount="940" columns="10">
+ <image source="tileset/zelda_oot v2.0.png" width="160" height="1504"/>
+</tileset>
---
+++ assets/maps/tiles_plain.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="tiles_plain" tilewidth="16" tileheight="16" tilecount="624" columns="26">
+ <image source="tileset\tiles.png" width="416" height="384"/>
+</tileset>
